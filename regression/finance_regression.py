208ec3b75822e5b3cb9cc5956ade022a2e86b26a
DhruvBajaj01 <71027506+DhruvBajaj01@users.noreply.github.com>
2021-09-03 12:18:36 +0530

    Update finance_regression.py

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -6,18 +6,16 @@
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
-
     Draws a little scatterplot of the training/testing data
-
     You fill in the regression code where indicated:
 """    
 
-
+%matplotlib inline 
 import sys
 import pickle
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+#sys.path.append("../../tools/")
+from feature_format import featureFormat, targetFeatureSplit 
+dictionary = pickle.load( open("../17. Final Project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -28,8 +26,8 @@ target, features = targetFeatureSplit( data )
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
-train_color = "b"
-test_color = "b"
+train_color = '#BBDEFB' #"b"
+test_color = '#FFCDD2'#"r"
 
 
 
@@ -37,12 +35,11 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (feature_train,target_train)
+print reg.coef_
+print reg.intercept_
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -57,13 +54,16 @@ plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
-
-
 ### draw the regression line, once it's coded
 try:
-    plt.plot( feature_test, reg.predict(feature_test) )
+    plt.plot( feature_test, reg.predict(feature_test))
 except NameError:
     pass
+
+# OUTLIERS
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r")
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
